d012501b031271c9ffee77d0e5fd433961bc08c2
whb <295851252@qq.com>
2018-11-19 05:46:00 -0800

    first add my http project

---
+++ test/test.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    std::string s("abcdefg");
+    std::cout << s.substr(4) << std::endl;
+
+//    char msg[] = "aaa bbb ccc";
+//    std::stringstream ss(msg);
+//    std::string p1, p2, p3;
+//
+//    ss >> p1 >> p2 >> p3;
+//
+//    std::cout << p1 << std::endl;
+    return 0;
+}
